8b8135e83b1b9101d66aa8c7c20433ce142aac43
Maltonn <ymatsubayashi09@outlook.jp>
2024-02-23 06:28:55 +0900

    fix2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/minor-circle-hp" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/hp" : "",
   trailingSlash: true,
 };
 
