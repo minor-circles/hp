412597b0c13fdaa7dfe61fa50a1b9039e640e1cf
Maltonn <ymatsubayashi09@outlook.jp>
2024-02-24 16:28:46 +0900

    edit icons

--- src/components/features/app/Seo/Seo.tsx
+++ src/components/features/app/Seo/Seo.tsx
@@ -5,7 +5,7 @@ import { joinPath } from '@/lib/joinPath';
 
 export const Seo = () => {
   const rootPath = useRootPath();
-  const imageURL = joinPath(ROOT_URL, '/assets/author.png');
+  const imageURL = joinPath(ROOT_URL, '/assets/thumbnail.png');
 
   return (
     <>
@@ -34,28 +34,11 @@ export const Seo = () => {
           cardType: 'summary_large_image',
         }}
         additionalLinkTags={[
-          { rel: 'icon', href: `${rootPath}/favicon.ico` },
-          {
-            rel: 'icon',
-            type: 'image/png',
-            sizes: '16x16',
-            href: `${rootPath}/favicons/favicon-16x16.png`,
-          },
           {
             rel: 'icon',
             type: 'image/png',
             sizes: '32x32',
             href: `${rootPath}/favicons/favicon-32x32.png`,
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '180x180',
-            href: `${rootPath}/favicons/apple-touch-icon-180x180.png`,
-          },
-          {
-            rel: 'mask-icon',
-            href: `${rootPath}/favicons/safari-pinned-tab.svg`,
-            color: '#5bbad5',
           },
         ]}
       />
