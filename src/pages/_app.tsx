551e002255bc76743e8c938036be902ee611abf3
Maltonn <ymatsubayashi09@outlook.jp>
2024-03-24 23:57:18 +0900

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,17 +9,34 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Seo />
-
       <div
         style={{ gridTemplateRows: 'auto 1fr auto' }}
         className="grid gap-10 min-h-screen bg-global"
       >
+
         <Header />
-        <ContentLayout className="px-0 py-6 sm:p-6">
+
+        <ContentLayout className="px-0 py-6 sm:p-6 z-0">
+          {/* <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
+            {new Array(10).fill(0).map((_, i) =>
+              <div key={i} style={{
+                position: 'absolute',
+                top: Math.random() * 100 + '%',
+                left: Math.random() * 100 + '%',
+                backgroundColor: 'red',
+                height: 'calc(60px / 2 * tan(60deg))',
+                width: '60px',
+                clipPath: 'polygon(50% 0, 100% 100%, 0 100%)',
+              }}></div>
+            )
+            }
+          </div> */}
           <Component {...pageProps} />
-        </ContentLayout>
-        <Footer />
-      </div>
+        </ContentLayout >
+        <div className='z-10'>
+          <Footer />
+        </div>
+      </div >
     </>
   );
 }
