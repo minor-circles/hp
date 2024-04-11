315a4450aa6e5feb215b8b41f9252d214c779e4d
Maltonn <ymatsubayashi09@outlook.jp>
2024-04-11 20:21:29 +0900

    add background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { ContentLayout } from '@/components/features/app/Layout';
 import { Seo } from '@/components/features/app/Seo';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const color_lst = ['#92B3DA', '#EECEE3', '#FBF5B9']
+  const randint = (max: number) => Math.floor(Math.random() * max)
   return (
     <>
       <Seo />
@@ -17,20 +19,22 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <Header />
 
         <ContentLayout className="px-0 py-6 sm:p-6 z-0">
-          {/* <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
-            {new Array(10).fill(0).map((_, i) =>
-              <div key={i} style={{
-                position: 'absolute',
-                top: Math.random() * 100 + '%',
-                left: Math.random() * 100 + '%',
-                backgroundColor: 'red',
-                height: 'calc(60px / 2 * tan(60deg))',
-                width: '60px',
-                clipPath: 'polygon(50% 0, 100% 100%, 0 100%)',
-              }}></div>
+          <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
+            {new Array(300).fill(0).map((_, i) =>
+              <div>
+                <svg width="100" height="100" style={{
+                  position: 'absolute',
+                  top: Math.random() * 150 - 50 + '%',
+                  left: Math.random() * 110 - 10 + '%',
+                  transform: `rotate(${Math.random() * 360}deg)`,
+                  transitionDuration: "3s",
+                }}>
+                  <path d="M50 0 L0 86 L100 86 Z" style={{ fill: "rgba(0,0,0,0)", stroke: color_lst[randint(color_lst.length)], strokeWidth: "2" }}></path>
+                </svg>
+              </div>
             )
             }
-          </div> */}
+          </div>
           <Component {...pageProps} />
         </ContentLayout >
         <div className='z-10'>
