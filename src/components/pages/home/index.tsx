8611d74d5a35c3d93e912bab45f89570b8fed261
meronking <151362045+meronking@users.noreply.github.com>
2024-04-12 18:16:09 +0900

    little updatate

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import {Link} from '@/components/common/Link';
+import { Link } from '@/components/common/Link';
 import { MainLayout } from '@/components/features/app/Layout';
 import { Profile } from '@/components/features/app/Profile';
 import { TopPost } from '@/components/features/post/TopPost';
@@ -12,14 +12,14 @@ export const Home: React.VFC<Props> = ({ posts }) => {
     <MainLayout
       main={
         <div>
-          {posts.map((post) => <TopPost post={post} key={post.title}/>)}
-          
+          {posts.map((post) => <TopPost post={post} key={post.title} />)}
+
           <div className="vstack gap-12 p-8 bg-primary-1">
             <h1 className="hstack gap-2 text-primary-1 text-3xl font-bold tracking-tighter leading-tight">マイナーサークルって何</h1>
             <p>
               一般に、全国的に珍しい活動を行っていたり慢性的に部員の少なかったりするサークルのことを指します。一風変わった個性豊かなサークルが集まっていることが特徴です。
               <br />
-              また、Twitter上でのみ活動するサークルも多く、Twitterサークルと呼ばれる場合もあります。
+              また、Twitter上でのみ活動するサークルも多くTwitterサークルと呼ばれる場合もあります。
               <br />
               最も、マイナーサークルの明確な定義はなく、日々意味合いは変化し続けています。
               <br />
@@ -29,7 +29,7 @@ export const Home: React.VFC<Props> = ({ posts }) => {
 
             <Link href='/circles/page/1/' passHref>
               <a className="button">
-              マイナーサークル一覧を見る
+                マイナーサークル一覧を見る
               </a>
             </Link>
             <p>※過去の大阪大学マイナーサークル合同説明会に参加したマイナーサークルも掲載しています。最新の情報ではないものもありますので、各ページの更新日時をご確認ください。</p>
@@ -37,7 +37,7 @@ export const Home: React.VFC<Props> = ({ posts }) => {
         </div>
 
       }
-      // aside={<Profile />}
+    // aside={<Profile />}
     />
   );
 };
