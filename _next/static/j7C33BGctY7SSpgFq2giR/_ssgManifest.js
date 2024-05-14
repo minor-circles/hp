53a5818b2f0abbfbd14a59e140ee4b93c6dce2a1
maltonn <maltonn@users.noreply.github.com>
2024-05-14 11:35:21 +0000

    deploy: f1c787f01f36b61ae8d77fad19434fcc34dbb27e

---
+++ _next/static/j7C33BGctY7SSpgFq2giR/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fposts","\u002Fcircles","\u002Ftags","\u002F","\u002Ftags\u002F[tag]","\u002Fcircles\u002Fpage\u002F[page]","\u002Fposts\u002Fpage\u002F[page]","\u002Fposts\u002F[slug]","\u002Fcircles\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
